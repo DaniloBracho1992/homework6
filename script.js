ce77c81f600ad7b24657a558cfda733bde86a2b3
Danilo Bracho Amesty <danilobrachoamesty@Danilos-MBP.cable.rcn.com>
2020-11-06 14:48:47 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// function to start displaying the webpage and definition of the variable
+
+function initPage() {
+    var input = document.getElementById("city input");
+    var searchEl = document.getElementById("search-button");
+    var clearEl = document.getElementById("clear-history");
+    var nameEl = document.getElementById("city-name");
+    var currentPicEl = document.getElementById("current-pic");
+    var currentTempEl = document.getElementById("temperature");
+    var currentHumidityEl = document.getElementById("humidity");4
+    var currentWindEl = document.getElementById("wind-speed");
+    var currentUVEl = document.getElementById("UV-index");
+    var historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+    
